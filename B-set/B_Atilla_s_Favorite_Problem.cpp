e3c6dda7faf840f3c4555c2872123cf40abab1c9
Ayush Balwani <ayushbalwani991@gmail.com>
2023-08-05 20:02:12 +0530

    B. Atilla's Favorite Problem

---
+++ B-set/B_Atilla_s_Favorite_Problem.cpp
@@ -0,0 +1,29 @@
+#include <bits/stdc++.h>
+using namespace std;
+
+#define endl '\n'
+
+#define int long long
+
+const int MOD = 1e9 + 7;
+const int INF = LLONG_MAX >> 1;
+
+signed main()
+{
+   ios::sync_with_stdio(false);
+   cin.tie(NULL);
+
+   int tc;
+   cin >> tc;
+   while (tc--)
+   {
+      int n;
+      cin >> n;
+      string str;
+      cin>>str;
+      sort(str.begin(),str.end());
+      cout<<str.back() - 'a' + 1 <<endl;
+   }
+
+   return 0;
+}
